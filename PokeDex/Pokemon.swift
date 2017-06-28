7221d43ce29728d10baf27b33bff78d8c6c0be18
Gilberto Pedro da Silva Júnior <gp7junior@gmail.com>
2017-06-27 23:50:30 -0300

    Added ParseCSV function

---
+++ PokeDex/Pokemon.swift
@@ -0,0 +1,30 @@
+//
+//  Pokemon.swift
+//  PokeDex
+//
+//  Created by Gilberto Pedro da Silva Jr on 27/06/17.
+//  Copyright © 2017 Alium. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    fileprivate var _name: String!
+    fileprivate var _pokedexId: Int!
+    
+    var name: String{
+        return _name
+    }
+    
+    var pokedexId: Int{
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+    
+        self._name = name
+        self._pokedexId = pokedexId
+    
+    }
+    
+}
